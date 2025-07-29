9c8ad4f673e819e82be4f1feb41a74f732a07926
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-07-30 01:03:39 +0530

    feat(analysis): enhance AnalysisPage with Usage component and video details display

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+import Usage from "@/components/Usage";
+import YoutubeVideoDetails from "@/components/YoutubeVideoDetails";
+import { FeatureFlag } from "@/features/flags";
+import { useParams } from "next/navigation";
+
 function AnalysisPage() {
+  const params = useParams<{ videoId: string }>();
+  const { videoId } = params;
+
   return (
     <div className="xl:container mx-auto px-4 md:px-0 ">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {/* Left side */}
         <div className="order-2 lg:order-1 flex flex-col gap-4 bg-white lg:border-r border-gray-200 p-6">
           {/* Analysis Section */}
+          <div className="flex flex-col gap-4 p-4 border border-gray-200 rounded-xl">
+            <Usage
+              featureFlag={FeatureFlag.ANALYSE_VIDEO}
+              title="Video Analysis"
+            />
+
+            {/* Video Transcription status  */}
+          </div>
           {/* Youtube Video Details */}
+          <YoutubeVideoDetails videoId={videoId} />
           {/* Thumbnail Generations */}
           {/* Titles Generation */}
           {/* Transcription Generation */}
