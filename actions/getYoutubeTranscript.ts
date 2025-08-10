c67a83474df09d9b9e9aeacac8e06beaf9f5bb23
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-08-10 23:38:35 +0530

    feat(transcripts): add Convex DB caching & analytics for YouTube transcript retrieval

--- actions/getYoutubeTranscript.ts
+++ actions/getYoutubeTranscript.ts
@@ -1,6 +1,11 @@
+import { api } from "@/convex/_generated/api";
+import { FeatureFlag, featureFlagEvents } from "@/features/flags";
+import { client } from "@/lib/schematic";
 import { currentUser } from "@clerk/nextjs/server";
+import { ConvexHttpClient } from "convex/browser";
 import { Innertube } from "youtubei.js";
 
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 export interface TranscriptEntry {
   text: string;
   timestamp: string;
@@ -43,10 +48,51 @@ export async function getYoutubeTranscript(videoId: string) {
 
   // TODO:Check if the transcript already exits in the database (is it cached?)
 
-  // If not, Fetch it from youtube
-  const transcript = await fetchTranscript(videoId);
-  return {
-    transcript,
-    cache: "This was not cached",
-  };
+  // Check if the transcript exits in database
+  const existingTranscript = await convex.query(
+    api.transcript.getTranscriptByVideoId,
+    { videoId, userId: user.id }
+  );
+
+  if (existingTranscript) {
+    console.log("🔍 Transcript found in database");
+    return {
+      cache:
+        "The video has already been transcribed - Accessing cached transcript instead of using a token",
+      transcript: existingTranscript,
+    };
+  }
+
+  try {
+    const transcript = await fetchTranscript(videoId);
+
+    // Store the transcript in the database
+    await convex.mutation(api.transcript.storeTranscript, {
+      videoId,
+      userId: user.id,
+      transcript,
+    });
+
+    await client.track({
+      event: featureFlagEvents[FeatureFlag.TRANSCRIPTION].event,
+      company: {
+        id: user.id,
+      },
+      user: {
+        id: user.id,
+      },
+    });
+
+    return {
+      transcript,
+      cache:
+        "The video has been transcribed - the transcript is now saved in the database and will be used in the future",
+    };
+  } catch (error) {
+    console.error("Error fetching transcript:", error);
+    return {
+      transcript: [],
+      cache: "Error fetching transcript, please try again later",
+    };
+  }
 }
