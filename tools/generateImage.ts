9aa8ae80dd046cb4a978a98cb708de1ecb33133f
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-08-17 23:17:15 +0530

    feat(ai-tools): add image generation tool with feature flag validation

---
+++ tools/generateImage.ts
@@ -0,0 +1,36 @@
+import { ImageGeneration } from "@/actions/imageGeneration";
+import { FeatureFlag } from "@/features/flags";
+import { client } from "@/lib/schematic";
+import { tool } from "ai";
+import z from "zod";
+
+const IMAGE_SIZE = "1792x1024" as const;
+
+export const generateImage = (videoId: string, userId: string) =>
+  tool({
+    description: "Generate an image",
+    parameters: z.object({
+      prompt: z.string().describe("The prompt to generate the image for"),
+      videoId: z.string().describe("The Youtube video ID"),
+    }),
+    execute: async ({ prompt }) => {
+      const schematicCtx = {
+        company: { id: userId },
+        user: {
+          id: userId,
+        },
+      };
+      const isImageGenerationEnabled = await client.checkFlag(
+        schematicCtx,
+        FeatureFlag.IMAGE_GENERATION
+      );
+
+      if (!isImageGenerationEnabled) {
+        return {
+          error: "Image generation is not enabled, the user must upgrade",
+        };
+      }
+      const image = await ImageGeneration(prompt, videoId);
+      return { image };
+    },
+  });
